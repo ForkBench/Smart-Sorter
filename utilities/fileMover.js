0f430b90289959d6b42c45ce32c83ee5f2dc23e6
Robin VAN DE MERGHEL <robin.van_de_merghel@etu.uca.fr>
2022-07-07 21:58:09 +0200

    Adding fileMover

---
+++ utilities/fileMover.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+const { printC, printCLn } = require("../utilities/printer");
+const path = require("path");
+
+module.exports = {
+    mover: (pathToPutImage, filePath, fileName) => {
+        // Check if the folder exists
+        if (!fs.existsSync(pathToPutImage)){
+            fs.mkdirSync(pathToPutImage, { recursive: true });
+        }
+
+
+        // If file already exists, rename it
+        var i = 1;
+        while (fs.existsSync(path.join(pathToPutImage, fileName))){
+            fileName = fileName.split(".");
+            fileName = fileName[0] + "(" + i + ")." + fileName[1];
+            i++;
+        }
+
+        if (i!==1){
+            printCLn(`\tRenamed ${fileName}`);
+        }
+
+        // Move the file to the right folder
+        fs.renameSync(filePath, pathToPutImage + fileName);
+
+
+        // Log
+        printCLn(`Moved ${fileName} to ${pathToPutImage}`, "yellow");
+    }
+}
