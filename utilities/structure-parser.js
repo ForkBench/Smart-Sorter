e61b395570a71894fe0d83db4bce3e2f6cfad7fb
ForkBench <robin.van_de_merghel@etu.uca.fr>
2022-07-16 10:17:47 +0200

    Updated structure parser, to list files instead of structuring it

--- utilities/structure-parser.js
+++ utilities/structure-parser.js
@@ -1,4 +1,6 @@
 const fs = require('fs');
+const { log } = require('../utilities/logger');
+const { printCLn, printC } = require('../utilities/printer');
 
 function tabulationAdder(n) {
     var str = "";
@@ -17,7 +19,7 @@ function getContent(content) {
 
 
     if (content === "*") {
-        return {value: "undefined", required: true};
+        return {value: "*", required: true};
     } else if (content.startsWith("?")) {
         return {value: getContent(content.substring(1, content.length)).value, required: false};
     }
@@ -29,50 +31,53 @@ function getContent(content) {
 
 
 function parser(lines) {
+
+    var paths = [];
+    var currentPath = ".";
+    var requiredFolder = true;
     
-    var structure = ['<?xml version=”1.0" encoding=”UTF-8"?>'];
-    var depth = 0;
     lines.forEach(line => {
-        line = emptySpaceRemover(line);
-        if (line.startsWith("[")) {
-            var data = line.substring(1, line.length - 2);
-            var content = getContent(data);
-            var spaces = tabulationAdder(depth);
-            structure.push(`${spaces}<folder name='${content.value}' required='${content.required}'>`);
-            depth++;
-        } else if (line.startsWith("}")) {
-            depth--;
-            var spaces = tabulationAdder(depth);
-            structure.push(`${spaces}</folder>`);
+        var clearedLine = emptySpaceRemover(line);
+        if (clearedLine.startsWith("[")) {
+            var name = getContent(clearedLine.substring(1, clearedLine.length - 2));
+            if (name.required) {
+                requiredFolder = true;
+                currentPath += "/" + name.value;
+            } else {
+                requiredFolder = false;
+            }
+        } else if (clearedLine.startsWith("}")) {
+            // We have to remove everything after the last "/"
+            if (requiredFolder === true) {
+                currentPath = currentPath.substring(0, currentPath.lastIndexOf("/"));
+            }
+            requiredFolder = true;
         } else {
-            var content = getContent(line);
-            var spaces = tabulationAdder(depth);
-            structure.push(`${spaces}<file name='${content.value}' required='${content.required}'/>`);
+            var content = getContent(clearedLine);
+            if (content.required) {
+                paths.push(currentPath + "/" + content.value);
+            }
         }
     });
-    return structure.join("\n");
+
+    return paths;
 
 }
 
 module.exports = {
     structureParser: (filePath) => {
 
+        printC(`Parsing structure file: ${filePath}...`);
+        log(`Parsing structure file: ${filePath}...`);
         const input = fs.readFileSync(filePath, 'utf8');
 
         const lines = input.split("\n");
 
         const data = parser(lines);
 
-        fs.writeFileSync(filePath.replace(new RegExp("st$"), 'xml'), data);
-        // Write lines on new xml file
+        printCLn(`Parsing structure file: ${filePath}... done.`);
+        log(`Parsing structure file: ${filePath}... done.`);
+        fs.writeFileSync(filePath.replace(new RegExp("st$"), 'txt'), data.join("\n"));
 
-        const xml2js = require('xml2js');
-        xml2js.parseString(data, (err, result) => {
-            if (err) {
-                console.log(err);
-            } else {
-                fs.writeFileSync(filePath.replace(new RegExp("st$"), 'json'), JSON.stringify(result, null, 2));
-            }
-        });
     }
 }
